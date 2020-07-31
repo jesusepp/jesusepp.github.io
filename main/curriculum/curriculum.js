965a6b65c071faf1c334b251bacc0183fb543691
jesusepp <jesusepereira@gmail.com>
2020-07-30 20:40:27 -0400

    Detalles

--- main/curriculum/curriculum.js
+++ main/curriculum/curriculum.js
@@ -1,8 +1,4 @@
-function selectMenu(n){
-    let j = document.getElementById('' + n);
-    if (j.style.maxHeight){
-        j.style.maxHeight = null;
-    } else {
-        j.style.maxHeight = j.scrollHeight + "px";
-    }
+let body = document.getElementsByTagName('body')[0];
+body.onload = function(){
+    insertHTML()
 }
