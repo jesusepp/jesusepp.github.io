3c689bdad3067af9e9354a5193cd5d9358456dac
jesusepp <jesusepereira@gmail.com>
2020-08-01 13:51:19 -0400

    Ajustes

--- main/contact-info/contact-info.js
+++ main/contact-info/contact-info.js
@@ -1,8 +1,19 @@
-function selectMenu(n){
-    let j = document.getElementById('' + n);
-    if (j.style.maxHeight){
-        j.style.maxHeight = null;
-    } else {
-        j.style.maxHeight = j.scrollHeight + "px";
-    }
+let body = document.getElementsByTagName('body')[0];
+
+body.onload = function(){
+    insertHTML(1);
+}
+
+let header = document.getElementsByTagName('header')[0];
+let footer = document.getElementsByTagName('footer')[0];
+let margin = document.getElementsByClassName('info-redes')[0];
+
+let totalHeigth = header.offsetHeight + body.offsetHeight + footer.offsetHeight;
+
+let windHeight = window.innerHeight;
+
+let difHeight = windHeight - totalHeigth;
+
+if (totalHeigth < windHeight){
+    margin.style.paddingBottom = 6*25 + difHeight + 'px';
 }
